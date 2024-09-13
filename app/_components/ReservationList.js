a81cf230aa8ed968f3191600bb8bd55b5ebda265
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-09-13 16:18:17 +0100

    Implement immediate reservation deletion by using useOptimistic hook

---
+++ app/_components/ReservationList.js
@@ -0,0 +1,32 @@
+"use client";
+
+import { deleteReservationAction } from "../_lib/actions";
+import ReservationCard from "./ReservationCard";
+import { useOptimistic } from "react";
+
+function ReservationList({ bookings }) {
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    (curBooking, bookingId) =>
+      curBooking.filter((booking) => booking.id !== bookingId)
+  );
+
+  async function handleDelete(bookingId) {
+    optimisticDelete(bookingId);
+    await deleteReservationAction(bookingId);
+  }
+
+  return (
+    <ul className="space-y-6">
+      {optimisticBookings.map((booking) => (
+        <ReservationCard
+          booking={booking}
+          key={booking.id}
+          onDelete={handleDelete}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ReservationList;
