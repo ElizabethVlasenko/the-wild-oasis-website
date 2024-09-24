e2771c60a1f0b9af6983cf9dc105a175389a0694
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-09-24 15:57:09 +0100

    Add edit button to the report

--- app/_components/ReportRow.js
+++ app/_components/ReportRow.js
@@ -1,9 +1,11 @@
+import { PencilSquareIcon } from "@heroicons/react/24/solid";
 import { format } from "date-fns";
+import Link from "next/link";
 
 const MESSAGE_LENGTH = 12;
 
 function ReportRow({ report }) {
-  const { created_at, subject, message } = report;
+  const { created_at, subject, message, id } = report;
   console.log(created_at);
 
   return (
@@ -32,7 +34,15 @@ function ReportRow({ report }) {
         </div>
       </div>
 
-      <div className="flex flex-col border-l border-primary-800 w-[100px]"></div>
+      <div className="flex flex-col border-l border-primary-800 w-[100px]">
+        <Link
+          href={`/account/report/edit/${id}`}
+          className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 border-b border-primary-800 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
+        >
+          <PencilSquareIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+          <span className="mt-1">Edit</span>
+        </Link>
+      </div>
     </div>
   );
 }
