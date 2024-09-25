1ab62a93ab3cb7a871ad9c42edcb4f19619c1fb0
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-09-25 15:37:58 +0100

    Add ability to show more/less of the message in the report table; css fixes

--- app/_components/ReportRow.js
+++ app/_components/ReportRow.js
@@ -1,16 +1,29 @@
-import { PencilSquareIcon } from "@heroicons/react/24/solid";
+"use client";
+
+import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/solid";
 import { format } from "date-fns";
 import Link from "next/link";
+import { useState } from "react";
 
-const MESSAGE_LENGTH = 12;
+const MESSAGE_LENGTH = 22;
 
 function ReportRow({ report }) {
   const { created_at, subject, message, id } = report;
-  console.log(created_at);
+  const [open, setOpen] = useState(false);
+
+  let displayMessage = "";
+  if (!open)
+    displayMessage =
+      message.split(" ").slice(0, MESSAGE_LENGTH).join(" ") +
+      (message.split(" ").length > MESSAGE_LENGTH ? "..." : "");
+
+  if (open) displayMessage = message;
+
+  //TODO status: submitted (can delete/edit the report), viewed, in progress, complete
 
   return (
     <div className="flex border border-primary-800">
-      <div className="flex-grow px-6 py-3 flex flex-col h-32">
+      <div className="flex-grow px-6 py-3 flex flex-col min-h-40">
         <div className="flex items-center justify-between">
           <h3 className="text-xl font-semibold capitalize">
             {subject.split("-").join(" ")}
@@ -23,12 +36,19 @@ function ReportRow({ report }) {
 
         <p className="text-lg text-primary-300"></p>
 
-        <div className="flex gap-5 mt-auto items-end">
-          <p className="text-lg text-primary-300  max-w-[500px]">
-            {message.split(" ").slice(0, MESSAGE_LENGTH).join(" ")}
-            {message.split(" ").length > MESSAGE_LENGTH && "..."}
+        <div className="flex-1 flex flex-col justify-between">
+          <p className="text-lg text-primary-300 mt-3 mb-2">
+            {displayMessage}{" "}
+            {message.split(" ").length > MESSAGE_LENGTH ? (
+              <button
+                onClick={() => setOpen(!open)}
+                className="text-accent-400"
+              >
+                [show {open ? "less" : "more"}]
+              </button>
+            ) : null}
           </p>
-          <p className="ml-auto text-sm text-primary-400">
+          <p className="text-right text-sm text-primary-400">
             Created {format(new Date(created_at), "EEE, MMM dd yyyy, p")}
           </p>
         </div>
@@ -42,6 +62,13 @@ function ReportRow({ report }) {
           <PencilSquareIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
           <span className="mt-1">Edit</span>
         </Link>
+        <button
+          href={`/account/report/edit/${id}`}
+          className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 border-b border-primary-800 flex-grow px-3 hover:bg-red-500 transition-colors hover:text-primary-900"
+        >
+          <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+          <span className="mt-1">Delete</span>
+        </button>
       </div>
     </div>
   );
