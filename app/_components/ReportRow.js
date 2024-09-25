a233004abee7e93f7278aeba0226a5f49d1d6732
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-09-25 15:49:25 +0100

    Add status to the report

--- app/_components/ReportRow.js
+++ app/_components/ReportRow.js
@@ -8,7 +8,7 @@ import { useState } from "react";
 const MESSAGE_LENGTH = 22;
 
 function ReportRow({ report }) {
-  const { created_at, subject, message, id } = report;
+  const { created_at, subject, message, id, status } = report;
   const [open, setOpen] = useState(false);
 
   let displayMessage = "";
@@ -23,15 +23,12 @@ function ReportRow({ report }) {
 
   return (
     <div className="flex border border-primary-800">
-      <div className="flex-grow px-6 py-3 flex flex-col min-h-40">
+      <div className="flex-grow px-6 py-3 flex flex-col min-h-40 max-w-[791px]">
         <div className="flex items-center justify-between">
           <h3 className="text-xl font-semibold capitalize">
             {subject.split("-").join(" ")}
           </h3>
-
-          <span className="bg-yellow-800 text-yellow-200 h-7 px-3 uppercase text-xs font-bold flex items-center rounded-sm">
-            status
-          </span>
+          <Status status={status} />
         </div>
 
         <p className="text-lg text-primary-300"></p>
@@ -55,23 +52,63 @@ function ReportRow({ report }) {
       </div>
 
       <div className="flex flex-col border-l border-primary-800 w-[100px]">
-        <Link
-          href={`/account/report/edit/${id}`}
-          className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 border-b border-primary-800 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
-        >
-          <PencilSquareIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
-          <span className="mt-1">Edit</span>
-        </Link>
-        <button
-          href={`/account/report/edit/${id}`}
-          className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 border-b border-primary-800 flex-grow px-3 hover:bg-red-500 transition-colors hover:text-primary-900"
-        >
-          <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
-          <span className="mt-1">Delete</span>
-        </button>
+        {status === "submitted" && (
+          <>
+            <Link
+              href={`/account/report/edit/${id}`}
+              className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 border-b border-primary-800 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
+            >
+              <PencilSquareIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+              <span className="mt-1">Edit</span>
+            </Link>
+            <button
+              href={`/account/report/edit/${id}`}
+              className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 border-b border-primary-800 flex-grow px-3 hover:bg-red-500 transition-colors hover:text-primary-900"
+            >
+              <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+              <span className="mt-1">Delete</span>
+            </button>{" "}
+          </>
+        )}
       </div>
     </div>
   );
 }
 
 export default ReportRow;
+
+function Status({ status }) {
+  console.log(status);
+  let statusStyle;
+  switch (status) {
+    case "complete": {
+      statusStyle = "bg-green-800 text-green-200";
+      break;
+    }
+    case "viewed": {
+      statusStyle = "bg-blue-800 text-blue-200";
+      break;
+    }
+    case "in-progress": {
+      statusStyle = "bg-yellow-800 text-yellow-200";
+      break;
+    }
+    case "submitted": {
+      statusStyle = "bg-purple-800 text-purple-200";
+      break;
+    }
+    default: {
+      statusStyle = "bg-yellow-800 text-yellow-200";
+    }
+  }
+
+  console.log(statusStyle);
+
+  return (
+    <span
+      className={`${statusStyle} h-7 px-3 uppercase text-xs font-bold flex items-center rounded-sm`}
+    >
+      {status.split("-").join(" ")}
+    </span>
+  );
+}
